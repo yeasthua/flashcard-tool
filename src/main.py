2fb6c093833f1d08e0d32af05f0aa55c1de9c7e8
yeshua <atrionsyeshua@gmail.com>
2026-08-12 21:23:45 +0800

    Refactor flashcard functionality: improve file reading, add flashcard creation and content deletion, and enhance user input handling

--- src/main.py
+++ src/main.py
@@ -1,18 +1,20 @@
-import random # For shuffling questions
+import random # For shuffling flashcards
 
 def read_file(filename: str) -> list[list]:
     "Reads the flashcard file, and then returns a list of lists which contains each line in the file (except header)"
-
-    with open(filename) as file:
+    
+    with open(filename, "r") as file:
         rows = []
         next(file)  # Skips header
     
         # Appends each question and answers into rows
         for line in file:
+            if line == "":  # Skips line that is empty
+                continue
     
             try:
                 rows.append(line.strip().split(";"))
-            except ValueError:  # Skips bad lines
+            except ValueError:  # Skips lines that has incorrect format
                 continue
     return rows
 
@@ -37,12 +39,53 @@ def run_quiz(filename: str):
         card_num += 1
 
         if user_answer.lower() == correct_answer.lower():
-            print("Correct!\n")
+            print("[Correct]\n")
             correct += 1
         else:
-            print("Incorrect\n")
+            print("[Incorrect]\n")
 
-    print(f"You scored {correct}/{len(questions_and_answers)}")
+    print(f"Total score: {correct}/{len(questions_and_answers)}")
+
+def write_flashcards():
+    filename = input("Enter new filename (.csv only): ")
+    if ".csv" not in filename:  # If file extension is not .csv
+        raise TypeError
+    
+    cards = []
+    cards.append("Question;Answer\n")     # Add header to flashcard
+
+    print("==== WRITE FLASHCARD  ====")
+    print("    Input '1' to exit   \n")
+    while True:
+        question = input("Question: ")
+        if question == '1':
+            break 
+
+        answer = input("Answer: ")
+        if answer == '1':
+            break
+        print()
+
+        cards.append(f"{question};{answer}\n")
+
+    return filename, cards
+
+def create_file(filename: str, cards: list):
+    "Creates a new flashcard file using the contents of the given list"
+
+    with open(filename, "w") as file:
+        for line in cards:
+            file.write(line)
+
+def delete_contents(filename: str):
+    "Deletes all contents of the given filename"
+    if ".csv" not in filename:
+        raise TypeError
+
+    with open(filename, "w") as file:
+        pass
+
+    print("STATUS: All contents erased!")
 
 
 def main():
@@ -53,18 +96,63 @@ def main():
     ╰╮╭┫┃┃╭╮┃━━┫╭╮┃╭━┫╭╮┃╭┫╭╮┣━━┫┃┃╭╮┃╭╮┃┃
     ╱┃┃┃╰┫╭╮┣━━┃┃┃┃╰━┫╭╮┃┃┃╰╯┣━━┫╰┫╰╯┃╰╯┃╰╮
     ╱╰╯╰━┻╯╰┻━━┻╯╰┻━━┻╯╰┻╯╰━━╯╱╱╰━┻━━┻━━┻━╯
-                                by yeasthua\n
+                                by yeasthua
     """)
 
     while True:
-        filename = input("+ Enter which file would you like to use (.csv files only): ")
+        #* Main menu
+        print("""
+        ========== FUNCTIONS ==========
+        (1): Run flashcard program
+        (2): Create a new flashcard file
+        (3): Erase all contents of an existing flashcard file
+        (4): Exit program
+        """)
 
+        # Handles invalid function input, eg: str or > 4
         try:
-            run_quiz(filename)
-            break
-        except FileNotFoundError:
-            print("File not found, please try again.\n")
+            user_input = int(input("Enter function: "))
+        except ValueError:
+            print("ERROR: Invalid Input\n")
             continue
-    
+        if user_input > 4 or user_input < 1:
+            print("ERROR: 1-4 only\n")
+            continue
 
+        #* Function 1
+        if user_input == 1:
+            print("====== FLASHCARD ======")
+            filename = input("Enter which file would you like to use (.csv files only): ")
+
+            try:
+                run_quiz(filename)
+            except FileNotFoundError:
+                print("ERROR: File not found")
+
+        #* Function 2
+        elif user_input == 2:
+            try:
+                file_name, flashcards = write_flashcards()
+                create_file(file_name, flashcards)
+            except TypeError:
+                print("ERROR: Invalid file extension")
+            except OSError:
+                print("ERROR: Cannot create file [Invalid filename]")
+
+        #* Function 3
+        elif user_input == 3:
+            print("==== ERASE CONTENTS ====")
+            try:
+                filename_delete = input("Enter filename to erase all content: ")
+                delete_contents(filename_delete)
+            except TypeError:
+                print("ERROR: Invalid file extension")
+            except OSError:
+                print("ERROR: Invalid filename")
+
+        #* Function 4
+        elif user_input == 4:
+            print("Terminating program...")
+            break
+    
 main()
